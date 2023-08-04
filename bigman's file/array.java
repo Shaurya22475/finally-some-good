94b70d2d04c655af6054a041a70b6039536373c9
Shaurya <shaurya22475@iiitd.ac.in>
2023-08-04 12:15:37 +0530

    this is epic

---
+++ bigman's file/array.java
@@ -0,0 +1,11 @@
+public class array {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! This is a simple greeting program.");
+        int i=0;
+        int j=8;
+        while (i<j){
+            System.out.print("This is my kingdom,"+i);
+            i++;
+        }
+    }
+}
